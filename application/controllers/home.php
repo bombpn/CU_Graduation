97a26c5eedc7ff660865745eda2606b669eab700
Tata201201 <tata201201@gmail.com>
2015-04-15 22:51:38 +0700

    Update Test Page

---
+++ application/controllers/home.php
@@ -0,0 +1,10 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class home extends CI_Controller {
+	public function index(){
+		$this->load->view('index');
+	}
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
