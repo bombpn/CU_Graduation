9f5a0ef0984b9d7c7de15c6d0b5f85a89ad87ceb
Bomb P. Nakwattananukul <bombpn@hotmail.com>
2015-04-27 01:39:18 +0700

    install PHPExcel into libraries, third_party

---
+++ application/controllers/report.php
@@ -0,0 +1,12 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class report extends CI_Controller {
+	public function __construct(){
+		parent::__construct();
+	}
+	public function index(){
+		//$this->load->view('inc_header');
+		//$this->load->view('report/Examples/01pharSimple');
+		//$this->load->view('inc_footer');
+	}
+}
