c296b7b14edb22af7f5c916d2f9a3d3701db9dbb
Bomb P. Nakwattananukul <bombpn@hotmail.com>
2015-04-29 22:47:08 +0700

    edit banner link in header

--- application/views/inc_header.php
+++ application/views/inc_header.php
@@ -55,7 +55,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">CU Graduation</a>
+                <a class="navbar-brand" href="<?=base_url(); ?>home/">CU Graduation</a>
             </div>
             <!-- Top Menu Items -->
             <!--ul class="nav navbar-right top-nav">
